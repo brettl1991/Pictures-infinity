b387b99b2868b00e67ecdb2b7eb1d8d7dc9a85c9
brettl1991 <brettl1991@gmail.com>
2023-03-17 13:22:46 +0000

    feat: add image and style it

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,13 @@ const HomePage = () => {
       <div className="loader">
         <Image src="/loader.svg" alt="Loading" height={100} width={100} />
       </div>
+      <div className="image-container">
+        <Image
+          fill
+          src="https://images.unsplash.com/photo-1677432658720-3d84f9d657b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80"
+          alt="tryingoutimage"
+        />
+      </div>
     </div>
   );
 };
